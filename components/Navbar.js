5ec774699d9b1416b73b907db03dd2b68440e3d4
jun9898 <nice1998@gmail.com>
2024-07-19 18:39:43 +0900

    Modify: 친구 아이콘 추가

--- components/Navbar.js
+++ components/Navbar.js
@@ -41,7 +41,7 @@ export default function Navbar({ userEmail, userNickname, userProfileImage, onNi
           <div className="flex items-center space-x-4">
             <button onClick={toggleFriendsList} className="text-gray-300 hover:text-white focus:outline-none">
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
               </svg>
             </button>
             <Link href="/" className="text-gray-300 hover:text-white font-semibold">
