92b5a297a896e74f0286e0414e487af060458fb1
jun9898 <nice1998@gmail.com>
2024-07-19 17:06:43 +0900

    Modify: title 중앙에 고정

--- components/Navbar.js
+++ components/Navbar.js
@@ -38,8 +38,8 @@ export default function Navbar({ userEmail, userNickname, userProfileImage, onNi
   return (
       <>
         <nav className="bg-gray-800 p-4 fixed w-full top-0 left-0 z-50 flex items-center justify-between shadow-md">
-          <div className="flex items-center">
-            <button onClick={toggleFriendsList} className="text-gray-300 hover:text-white focus:outline-none mr-4 z-50">
+          <div className="flex items-center space-x-4">
+            <button onClick={toggleFriendsList} className="text-gray-300 hover:text-white focus:outline-none">
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
               </svg>
@@ -48,22 +48,22 @@ export default function Navbar({ userEmail, userNickname, userProfileImage, onNi
               Main
             </Link>
           </div>
-          <div className="text-white text-lg font-bold">
+          <div className="text-white text-lg font-bold absolute left-1/2 transform -translate-x-1/2">
             {title}
           </div>
-          <div className="flex items-center">
+          <div className="flex items-center space-x-4">
             {userProfileImage && (
                 <img
                     src={userProfileImage}
                     alt={`${userNickname}'s profile`}
-                    className="w-8 h-8 rounded-full mr-2"
+                    className="w-8 h-8 rounded-full"
                 />
             )}
             <Link href={`/myPage/${encodeURIComponent(userEmail)}`} className="text-gray-300 hover:text-white font-semibold">
               {userNickname}
             </Link>
             <button
-                className="text-gray-300 hover:text-white ml-4 font-semibold"
+                className="text-gray-300 hover:text-white font-semibold"
                 onClick={handleLogout}
             >
               로그아웃
